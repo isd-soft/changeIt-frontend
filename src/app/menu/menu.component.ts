34fac3ccfa2e0dcaf76b0d321ee672fcc0507bbd
Catalin <ghermancatalin4@gmail.com>
2020-11-13 10:26:27 +0200

    initial comit

---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import {User} from '@app/models';
+import {AccountService} from '@app/services';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements OnInit {
+
+  user: User;
+
+  constructor(private accountService: AccountService) {
+    this.accountService.user.subscribe(x => this.user = x);
+  }
+
+  ngOnInit(): void {
+  }
+
+  logout() {
+    this.accountService.logout();
+  }
+}
---
+++ src/app/models/user.ts
@@ -0,0 +1,7 @@
+﻿export class User {
+    id: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    token: string;
+}
