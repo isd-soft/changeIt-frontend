e25847b6ab39fd0f22efdfe81274efb49d91b85f
Catalin <ghermancatalin4@gmail.com>
2020-11-25 11:39:44 +0200

    fix some errors

--- src/app/statistics/statistic.component.ts
+++ src/app/statistics/statistic.component.ts
@@ -10,7 +10,7 @@ import {ProblemService} from '@app/service/problem.service';
 })
 export class StatisticComponent implements OnInit {
 
-  private problems: Problem[];
+  public problems: Problem[];
 
   constructor(private problemService: ProblemService,) {
     problemService.getData().subscribe(data => this.problems = data);
